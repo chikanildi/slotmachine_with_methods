799a5c1648b9ab3b146dd8471f32179e5e593a8d
code submits <chikanildi@hotmail.com>
2023-03-25 15:44:51 +0100

    Update ConsoleMethods.cs
    updated the ConsoleMethods.cs

--- ConsoleMethods.cs
+++ ConsoleMethods.cs
@@ -1,11 +1,74 @@
 ﻿using System;
-namespace slotmachine_with_methods
-{
-	public class EmptyClass
-	{
-		public EmptyClass()
-		{
-		}
-	}
-}
 
+class SlotMachineUI
+{
+    public static void WelcomeMessage(int coins)
+    {
+        Console.WriteLine("Welcome to the Slot Machine game!");
+        Console.WriteLine($"You have {coins} coins");
+    }
+
+    public static int GetBetAmount(int minBet, int maxBet)
+    {
+        Console.WriteLine($"How much would you like to bet? ({minBet}-{maxBet})");
+        Console.Write("Bet: ");
+        int betAmount = int.Parse(Console.ReadLine());
+        return betAmount;
+    }
+
+    public static void DisplaySpin(int[,] spin)
+    {
+        for (int i = 0; i < spin.GetLength(0); i++)
+        {
+            for (int j = 0; j < spin.GetLength(1); j++)
+            {
+                Console.Write($"{spin[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void DisplayWinnings(int matches, int winnings)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"The number of matches: {matches}");
+        Console.WriteLine($"You won {winnings} dollars!");
+    }
+
+    public static void DisplayCoins(int coins)
+    {
+        Console.WriteLine($"You have {coins} coins");
+    }
+
+    public static bool PlayAgainPrompt()
+    {
+        Console.Write("Would you like to play again? (y/n) ");
+        string playAgainStr = Console.ReadLine();
+
+        if (playAgainStr.ToLower() == "y")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void InvalidBetMessage()
+    {
+        Console.WriteLine("Invalid bet amount. Please try again.");
+    }
+
+    public static void NotEnoughCoinsMessage()
+    {
+        Console.WriteLine("You don't have enough coins to place that bet.");
+    }
+
+    public static void GameOverMessage()
+    {
+        Console.WriteLine("You are out of coins");
+        Console.WriteLine("\nSorry, you lost.");
+        Console.ReadLine();
+    }
+}
